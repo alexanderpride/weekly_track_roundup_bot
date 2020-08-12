4b4d9780641566448f46baf2d089148f95ced44e
Alexander <alexandercpride@gmail.com>
2020-08-12 19:14:07 +1200

    Is working, adds songs to playlist and lists songs that cannot be found. Still need to add some features, like dynamically changing the cover image, automatically running the program and better song searching

--- main.py
+++ main.py
@@ -8,114 +8,139 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def getChanel():
-    # Gets theneedledrop's chanel and returns it
+class Bot:
 
-    req = requests.get("https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=theneedledrop&key=" + os.getenv("API_KEY"))
+    def __init__(self):
 
-    if req.status_code == 200:
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public", username="Alexander Pride"))
 
-        return req.json()["items"][0]
+    def getWTRDesc(self):
 
-    else:
+        playlist_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLP4CSgl7K7or84AAhr7zlLNpghEnKWu2c&key=" + os.getenv("API_KEY")
 
-        return None
+        res = requests.get(playlist_url)
 
+        if res.status_code == 200:
 
-def getUploads(chanel):
-    # Gets the playlistID for the chanels uploads so that the most recent video can be returned
+            latest_video = res.json()["items"][0]
+            print(latest_video)
+            latest_video_description = latest_video["snippet"]["description"]
 
-    uploads_url = chanel["contentDetails"]["relatedPlaylists"]["uploads"]
+            print(latest_video_description)
 
-    req = requests.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=' + uploads_url + '&key=' + os.getenv("API_KEY"))
-
-    if req.status_code == 200:
-
-        return req.json()["items"]
-
-
-def getVideoDesc(uploads):
-    # Returns the description from the Weekly Track Roundup video or None if it's not found
-
-    for upload in uploads:
-
-        if re.match(r'([Ww]eekly [Tt]rack [Rr]oundup)', upload["snippet"]["title"]):
-
-            return upload["snippet"]["description"]
-
-    return None
-
-
-def getSongs(desc):
-
-    songs = re.findall(r'.+ - .+', desc)
-
-    return songs
-
-
-def getSpotipyObject():
-
-    scope = "playlist-modify-public"
-
-    return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username="Alexander Pride"))
-
-def getSongIDs(sp, songs):
-
-    songIDs = []
-    print("Songs in Video Desc: " + str(len(songs)))
-
-    for song in songs:
-
-        # Replace all the parts that Spotify struggles to parse and format string for search
-
-        _song = song.replace("&", "")
-        _song = _song.replace("x", "")
-        _song = _song.replace("ft.", "")
-        _song = _song.replace(" ", "+")
-        print(_song)
-
-        results = sp.search(_song, type="track")
-
-        tracks = results["tracks"]
-
-        # CHeck if there was a song returned
-        if tracks["total"] > 0:
-
-            trackItems = tracks["items"]
-
-            songIDs.append(trackItems[0]["id"])
-
-            # TODO implement checking to always add the explicit song
-            # # Check if there are two versions of the same track with and without explicit lyrics, if so add the one with
-            # # explicit lyrics
-            # if len(trackItems) == 2 and \
-            #     trackItems[0]["name"] == trackItems[1]["name"] and \
-            #     trackItems[0]["explicit"] != trackItems[1]["explicit"]:
+            return latest_video_description
 
         else:
 
-            print("\n----------\n")
+            print("Couldn't access the YouTube API")
+            print("Status code", res.status_code)
+            print(res.json())
+
+    def getSongs(self, desc):
+
+        songs = re.findall(r'.+ - .+', desc)
+
+        return songs
+
+    def getSearchTerm(self, song):
+        """
+        The format of the song desc can have a few different formats
+        A couple of examples are...
+
+        Anderson .Paak - Lockdown (Remix) ft. Noname, JID & Jay Rock
+        Troye Sivan - Rager teenager!
+        Machine Gun Kelly & blackbear - my ex's best friend
+
+        There are three main categories I can determine in the song desc
+
+        1: The artists that the song belongs to, on the left of the ' - '
+        2: The name of the song, on the right of the ' - '
+        3: The artists that feature on the song, after the 'ft.'
+        """
+
+        # Splits the song into category 1, and a joined category 2 and 3
+        artists, song = song.split(" - ")
+        artists = re.split(r'[,&]', artists)
+
+        # Separate 2 and 3 into their own categories, and create a list of features
+        if "ft." in song:
+            song, features = song.split("ft.")
+            features = re.split(r'[,&]', features)
+
+        # Create a search string with the song name and artists included
+        search_term = song + " "
+
+        for artist in artists:
+            search_term += artist
+
+        return search_term
+
+    def getSongIDs(self, songs):
+
+        songIDs = []
+        unfoundSongs = []
+        print("Songs in Video Desc: " + str(len(songs)))
+
+        for song in songs:
+
+            # Replace all the parts that Spotify struggles to parse and format string for search
+
+            print()
+            print("Song searching for:")
             print(song)
-            print()
-            print(tracks)
-            print()
-            print("\n----------\n")
 
-    print("Songs found by Spotify: " + str(len(songIDs)))
-    return songIDs
+            search_term = self.getSearchTerm(song)
 
+            print("Using search term:")
+            print(search_term)
+
+            results = self.sp.search(search_term, type="track")
+
+            tracks = results["tracks"]
+
+            # CHeck if there was a song returned
+            print("Song found:")
+            if tracks["total"] > 0:
+
+                trackItems = tracks["items"]
+
+                print(trackItems[0]["artists"][0]["name"] + ", " + trackItems[0]["name"])
+
+                songIDs.append(trackItems[0]["id"])
+
+            else:
+
+                unfoundSongs.append(song)
+
+        print()
+        print("Number of songs found by Spotify: " + str(len(songIDs)))
+        print("Couldn't find:")
+
+        for song in unfoundSongs:
+            print(song)
+
+        return songIDs
+
+    def addToPlaylist(self, songIDs):
+
+        """5kCKSF7HlIMjlIrStlCCra"""
+
+        try:
+
+            self.sp.user_playlist_replace_tracks(self.sp.current_user()["id"], "5kCKSF7HlIMjlIrStlCCra", songIDs)
+
+        except:
+
+            print("Error adding items to playlist")
 
 
 def main():
 
-    sp = getSpotipyObject()
-    chanel = getChanel()
-    uploads = getUploads(chanel)
-    desc = getVideoDesc(uploads)
-    songs = getSongs(desc)
-
-    songIDs = getSongIDs(sp, songs)
-
+    bot = Bot()
+    desc = bot.getWTRDesc()
+    songs = bot.getSongs(desc)
+    songIDs = bot.getSongIDs(songs)
+    bot.addToPlaylist(songIDs)
 
 
 if __name__ == '__main__':
