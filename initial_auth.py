0ae8142c8667502f0da4e56e07ecc8f62fdf1b7c
alexa <alexandercpride@gmail.com>
2021-02-09 20:04:43 +1300

    Called function lmao

--- initial_auth.py
+++ initial_auth.py
@@ -6,3 +6,6 @@ def init_auth():
     """Connect to spotify to authenticate use of the users account"""
 
     spotipy.SpotifyOAuth(scope="playlist-modify-public", username=os.getenv("SPOTIPY_USERNAME"))
+
+
+init_auth()
