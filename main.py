cf9edd45949777e98dde5c813caf6c58d27a1ed8
alexa <alexandercpride@gmail.com>
2021-02-09 19:58:31 +1300

    Changed main to function slightly differently

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 scheduler = BlockingScheduler()
 
 
-def init_run():
-
-    bot = Bot()
-    bot.force_run()
-
-
 @scheduler.scheduled_job('interval', hours=6)
 def timed_run():
 
@@ -19,6 +13,5 @@ def timed_run():
 
 if __name__ == '__main__':
 
-    init_run()
     scheduler.start()
 
