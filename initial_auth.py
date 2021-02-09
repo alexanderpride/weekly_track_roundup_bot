e41677a0ea2b9f18934c5282afa7fa85b859dfe9
alexa <alexandercpride@gmail.com>
2021-02-09 20:03:51 +1300

    Added a function to create a cache on hosted site

---
+++ initial_auth.py
@@ -0,0 +1,8 @@
+import spotipy
+import os
+
+
+def init_auth():
+    """Connect to spotify to authenticate use of the users account"""
+
+    spotipy.SpotifyOAuth(scope="playlist-modify-public", username=os.getenv("SPOTIPY_USERNAME"))
